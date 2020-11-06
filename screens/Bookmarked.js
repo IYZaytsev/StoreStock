7af59bbc921ddb9fa605bbc0275328fd6a5885a8
Ivan <IvanZaytsev123@gmail.com>
2020-11-05 19:07:53 -0500

    messing around with flatlist alphabet component

--- screens/Bookmarked.js
+++ screens/Bookmarked.js
@@ -1,9 +1,32 @@
 import * as React from "react";
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import AlphabetList from "react-native-flatlist-alphabet";
 export default function Bookmarked(props) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Bookmarked screen place holder</Text>
-      </View>
-    );
-  }
+  // Using https://www.npmjs.com/package/react-native-flatlist-alphabet component
+  const YourData = [
+    {
+      value: "Apple",
+      key: "A",
+    },
+    {
+      value: "pear",
+      key: "B",
+    },
+    {
+      value: "peach",
+      key: "C",
+    },
+    {
+      value: "watermelon",
+      key: "D",
+    },
+  ];
+
+  return (
+
+      <AlphabetList
+        data={YourData}
+      />
+  );
+}
+
