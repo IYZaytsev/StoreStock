64b43ebe7a4c5905dab494cc588d0dacfa0f7651
Hayden Key <hwkey.77@gmail.com>
2020-11-19 22:51:09 -0500

    added graph component

--- components/StockGraph.jsx
+++ components/StockGraph.jsx
@@ -1,23 +1,69 @@
 import * as React from "react";
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeigth = Dimensions.get("window").height;
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100
+      ]
+    }
+  ]
+};
+
+const chartConfig = {
+  backgroundColor: "#FBFBF8",
+  backgroundGradientFrom: "#FBFBF8",
+  backgroundGradientTo: "#FBFBF8",
+  decimalPlaces: 2, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(61, 136, 136, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+};
+
+
 export default function StockGraph() {
   // Example of json for five day price
   /*
     (last entry in the array is current day)
-    "5day_prices":[
-    {"close":49.900001525878906,"open":49.810001373291016},
-    {"close":47.959999084472656,"open":49.0},
-    {"close":48.02000045776367,"open":47.72999954223633},
-    {"close":48.060001373291016,"open":47.810001373291016},
-    {"close":48.63999938964844,"open":48.75}
-  ]
+    "5day_prices":
+    [
+      {"close":49.900001525878906,"open":49.810001373291016},
+      {"close":47.959999084472656,"open":49.0},
+      {"close":48.02000045776367,"open":47.72999954223633},
+      {"close":48.060001373291016,"open":47.810001373291016},
+      {"close":48.63999938964844,"open":48.75}
+    ]
   
-  // For the non graph fields
+    // For the non graph fields
     I have "fiftyTwoWeekHigh":60.13,"fiftyTwoWeekLow":36.27,"marketCap":209026990080
   */
     return (
-      <View style={styles.border}>
-        <Text style={styles.colorfulText}>StockGraph Componenet</Text>
+      <View>
+        <LineChart
+          data={data}
+          width={screenWidth} // from react-native
+          height={(screenHeigth * .75)}
+          yAxisLabel="$"
+          yAxisInterval={2} // optional, defaults to 1
+          chartConfig={chartConfig}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
       </View>
     );
   }
--- screens/StockGraphTest.js
+++ screens/StockGraphTest.js
@@ -1,14 +1,11 @@
 import * as React from "react";
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 import StockGraph from "../components/StockGraph"
-export default function StockGraphTest(props) {
-    
+
+export default function StockGraphTest(props) {    
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-
-        <Text>Stock Graph work area </Text>
-        <StockGraph
-        />
+        <StockGraph />
       </View>
     );
   }
